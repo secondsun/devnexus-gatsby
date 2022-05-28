08aa79681ac286b84e7c694cc42aca7639735f65
Hoyt Summers Pittman <secondsun@gmail.com>
2022-05-28 12:29:56 -0400

    Adding PWA support and updates

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,9 +3,23 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `DevNexus 2023`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://novel-dew.cloudvent.net/`
   },
-  plugins: ["gatsby-plugin-sass"]
+  plugins: ["gatsby-plugin-sass",
+            {
+              resolve: `gatsby-plugin-manifest`,
+              options: {
+                icon: 'src/images/icon.png',
+                name: `DevNexus 2023`,
+                short_name: `DevNexus`,
+                start_url: `/`,
+                background_color: `#fcfcfc`,
+                theme_color: `#006e00`,
+                display: `standalone`,
+
+              }
+            },
+            'gatsby-plugin-offline']
 };
 
 export default config;

e7a5a5735fbd670311e96d98465b144bd6417ab3
Hoyt Summers Pittman <secondsun@gmail.com>
2022-05-28 12:40:02 -0400

    display message when update service worker

---
+++ gatsby-browser.tsx
@@ -0,0 +1,11 @@
+const React = require("react")
+
+exports.onServiceWorkerUpdateReady = () => {
+    const answer = window.confirm(
+      `This application has been updated. ` +
+        `Reload to display the latest version?`
+    )
+    if (answer === true) {
+      window.location.reload()
+    }
+  }
